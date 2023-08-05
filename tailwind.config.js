3301a808dad2754162f2c93da690e9ad1f7222ad
jpbs-developer <jpbs1@aluno.ifal.edu.br>
2023-08-05 14:03:09 -0300

    chore: add tailwindcss and angular material

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        green: "#00875F",
+        "black-100": "#29292E",
+        "black-200": "#121214",
+      },
+    },
+  },
+  plugins: [],
+};
